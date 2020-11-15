b84709d3a7e78b0def854f50d0d52b405753c11d
James Foster <github@jgfoster.net>
2020-11-14 22:17:26 -0800

    Use `Adafruit BusIO` as name for library.

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -5,4 +5,4 @@ bundle exec ensure_arduino_installation.rb
 
 # get custom version of Adafruit_BusIO
 git clone https://github.com/Arduino-CI/Adafruit_BusIO.git
-mv Adafruit_BusIO $(bundle exec arduino_library_location.rb)
+mv Adafruit_BusIO "$(bundle exec arduino_library_location.rb)/Adafruit BusIO"
