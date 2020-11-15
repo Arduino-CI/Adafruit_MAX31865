205037dc5abfbdfb44d27be1b9973213a37e89f2
James Foster <github@jgfoster.net>
2020-11-14 22:06:09 -0800

    Use recent `arduino_ci` for testing; move scripts to new directory

---
+++ scripts/install_libraries.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set up directories
+bundle exec ensure_arduino_installation.rb
+
+# get custom version of Adafruit_BusIO
+git clone https://github.com/Arduino-CI/Adafruit_BusIO.git
+mv Adafruit_BusIO $(bundle exec arduino_library_location.rb)
