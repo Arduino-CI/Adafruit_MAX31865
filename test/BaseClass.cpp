5ec2731f77013148aa825d71ccb8f79a84dbb341
Caleb Herbel <36583106+cherbel@users.noreply.github.com>
2020-11-23 11:54:31 -0800

    Added Arduino CI integration (#6)
    * Finnishing up High Level Tests
    * Minor Formatting issues from Code Review
    * Use recent `arduino_ci` for testing; move scripts to new directory
    * Use `Adafruit BusIO` as name for library.
    * Compiles with up-to-date Bus-IO
    * Add libraries

---
+++ test/BaseClass.cpp
@@ -0,0 +1,52 @@
+#define Adafruit_MAX31865_Test Adafruit_MAX31865_Base
+#include "Common.cpp"
+
+unittest(testingClassName) {
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  assertEqual("Adafruit_MAX3186_Base", thermo.className());
+}
+
+// The following tests are in BaseClass.cpp instead of Common.cpp
+// because initial input values for resistance and fault from the
+// MAX31865 are initialized differently for testing purposes.
+// Since we don't have actual I/O from the device, we need to
+// initialize values in testing so we can manually change
+// them.
+
+unittest(read_RTD) {
+  vector<int> expected{0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0,
+                       2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2,
+                       2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                       2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2,
+                       2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  BitCollector pinValues(false);
+  thermo.readRTD();
+  assertTrue(pinValues.isEqualTo(expected));
+}
+
+unittest(read_fault) {
+  vector<int> expected{0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  BitCollector pinValues(false);
+  uint8_t fault = thermo.readFault();
+  assertEqual(0, fault);
+  assertTrue(pinValues.isEqualTo(expected));
+}
+
+unittest(clear_fault) {
+  vector<int> expected{0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2,
+                       2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2};
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  BitCollector pinValues(false);
+  thermo.clearFault();
+  assertTrue(pinValues.isEqualTo(expected));
+}
+
+unittest_main()
---
+++ test/TestClass.cpp
@@ -0,0 +1,57 @@
+#define Adafruit_MAX31865_Test Adafruit_MAX31865
+#include "Common.cpp"
+
+unittest(testingClassName) {
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  assertEqual("Adafruit_MAX3186_CI", thermo.className());
+}
+
+unittest(begin_high) {
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  bool testBegin = thermo.begin(MAX31865_3WIRE);
+  assertTrue(testBegin);
+}
+
+unittest(readRTD_high) {
+  uint16_t expectedRTD = 0;
+  uint16_t expectedSetRTD = 3;
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  uint16_t testRTD = thermo.readRTD();
+  assert(testRTD == expectedRTD);
+  thermo.setRTD(expectedSetRTD);
+  testRTD = thermo.readRTD();
+  assert(testRTD == expectedSetRTD);
+}
+
+unittest(getTemp_high) {
+  float expectedTemp = -242.02;
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  float testTemp = thermo.temperature(100.0, 430.0);
+  assert(testTemp == expectedTemp);
+}
+
+unittest(readFault_high) {
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  uint8_t expectedFault = 1;
+  uint8_t testFault = thermo.readFault();
+  assertEqual(0, testFault);
+  thermo.setFault(expectedFault);
+  testFault = thermo.readFault();
+  assert(testFault == expectedFault);
+}
+
+unittest(clearFault_high) {
+  uint8_t newFault = 1;
+  uint8_t expectedFault = 0;
+  Adafruit_MAX31865_Test thermo(ss, mosi, miso, sck);
+  thermo.begin(MAX31865_3WIRE);
+  thermo.setFault(newFault);
+  thermo.clearFault();
+  uint8_t testFault = thermo.readFault();
+  assert(testFault == expectedFault);
+}
+
+unittest_main()
