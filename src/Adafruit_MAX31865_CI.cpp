8fcc0184bab75fb8494f6a0eb15dbdb706832ab8
James Foster <git@jgfoster.net>
2021-03-22 14:18:33 -0700

    Add ability to set temperature in CI (to support a test double).

--- src/Adafruit_MAX31865_CI.cpp
+++ src/Adafruit_MAX31865_CI.cpp
@@ -6,6 +6,7 @@
 #include <pgmspace.h>
 #endif
 
+#include <cassert>
 #include <stdlib.h>
 
 Adafruit_MAX31865_CI::Adafruit_MAX31865_CI(int8_t spi_cs, int8_t spi_mosi,
@@ -19,9 +20,27 @@ bool Adafruit_MAX31865_CI::begin(max31865_numwires_t wires) {
   return Adafruit_MAX31865_Base::begin(wires);
 }
 
-// BAD: see https://github.com/Arduino-CI/Adafruit_MAX31865/issues/7
 float Adafruit_MAX31865_CI::temperature(float RTDnominal, float refResistor) {
   return Adafruit_MAX31865_Base::temperature(RTDnominal, refResistor);
+}
+
+void Adafruit_MAX31865_CI::setTemperature(float newTemp) {
+  const int RTDnominal = 100;
+  const int refResistor = 430;
+  assert(0 <= newTemp && newTemp <= 100);
+  int low = 7000;
+  int high = 10700;
+  int mid = (low + high) / 2;
+  while (low + 1 < high) {
+    setRTD(mid);
+    float temp = temperature(RTDnominal, refResistor);
+    if (temp <= newTemp) {
+      low = mid;
+    } else {
+      high = mid;
+    }
+    mid = (low + high) / 2;
+  }
 }
 
 #endif
